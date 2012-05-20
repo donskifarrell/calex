7a6e34cb9a0ec3e86601d7009077df26f52c5fb1
donskifarrell <donskifarrell@googlemail.com>
2012-05-20 22:31:01 +0100

    Added yaml for github repo details. Updated where to save + how to build new X entries for commiting to Github. Added simple style for calendar.

--- src/javascript/calex.js
+++ src/javascript/calex.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
-  var dayContent = '';
+  gh = new Github({
+    repositoryRoot : $('#_repository').val(),
+    defaultCommitPath : $('#_commitPath').val(),
+    defaultCommitMessage : $('#_commitMessage').val() 
+  });
+  builder = new EntryBuilder();
 
-  $('#calendar').fullCalendar({
+  var dayContent = '';
+  var $calendar = $('#calendar');
+  var $main = $('#main');
+
+  var $entryMarkdown = $('#entryMarkdown');
+  var $entryPreview = $('#entryPreview').hide();
+  var $entryTitle = $('#entryTitle');
+  var $commitBtn = $('#commitButton');
+  var $usernameField = $('#username');
+  var $passwordField = $('#password');
+
+  $calendar.fullCalendar({
     header: {
       left: 'prev,next today',
       center: 'title',
       right: ''
     },
+    height: 400,
+    width: 100,
     editable: true,
     dayRender: function(cell, date) {
       var day = date.format("yyyy-mm-dd");
       var entries = dayEntries[day];
       if (entries !== undefined){
-        cell.addClass('fc-xdate')
-            .append('<img src="../images/x.png">');
+        cell.addClass('fc-xdate');
         cell.click(function() {
           showDayEntries(entries);
         });
@@ -22,7 +39,7 @@ $(document).ready(function() {
   });
 
   function showDayEntries(entries){      
-    $('#main').ajaxStop(function(){
+    $main.ajaxStop(function(){
       $.facebox(dayContent);
     });
     getContentOfEntries(entries);
@@ -46,4 +63,34 @@ $(document).ready(function() {
 
     $.when.apply($, entry_calls);
   }
+
+  $commitBtn.click(function(e){
+    e.preventDefault();
+    entry = builder.buildEntry($entryTitle.val(), $entryMarkdown.val());
+    gh.setCredentials($usernameField.val(), $passwordField.val());
+    gh.commit(entry);
+  });
 });
+
+var EntryBuilder = function() {
+  var date = new Date();
+  YAML_FRONT_MATTER = '---\n' +
+                      'title: %title\n' +
+                      '---\n\n';
+
+  this.buildEntry = function(title, data) {
+      filename = title.replace(/\s+/g, '-').toLowerCase();
+      yaml = YAML_FRONT_MATTER.replace('%title', title);
+      
+      entry = {
+          filename: buildDate(new Date()) + '-' + filename + '.md',
+          body: yaml + data
+      };
+
+      return entry;
+  }
+}
+
+function buildDate(date){
+  return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
+}
